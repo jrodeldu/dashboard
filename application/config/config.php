965b3ae6c5e3507e04015b2dc35127423af56ed0
Johnny <jrodeldu@gmail.com>
2013-01-03 23:31:14 +0000

    Crear Vhost y quitar ruta local

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/jrodeldu_modules';
+$config['base_url']	= 'http://dashboard.dev';
 
 /*
 |--------------------------------------------------------------------------
